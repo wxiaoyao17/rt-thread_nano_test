718907c7edb918ce2ecf3a748cfa1a85aa7debd5
wxiaoyao17 <365723488@qq.com>
2020-04-11 17:31:08 +0800

    修改IWDG驱动

--- stm32g071-st-nucleo/Hardware/Src/iwdg.c
+++ stm32g071-st-nucleo/Hardware/Src/iwdg.c
@@ -1,16 +1,17 @@
+#include "config.h"
 #include "iwdg.h"
-//////////////////////////////////////////////////////////////////////////////////	 
+//////////////////////////////////////////////////////////////////////////////////
 //本程序只供学习使用，未经作者许可，不得用于其它任何用途
 //ALIENTEK STM32F407开发板
-//独立看门狗驱动代码	   
+//独立看门狗驱动代码
 //正点原子@ALIENTEK
 //技术论坛:www.openedv.com
 //创建日期:2017/4/7
 //版本：V1.0
 //版权所有，盗版必究。
 //Copyright(C) 广州市星翼电子科技有限公司 2014-2024
-//All rights reserved									  
-////////////////////////////////////////////////////////////////////////////////// 
+//All rights reserved
+//////////////////////////////////////////////////////////////////////////////////
 
 IWDG_HandleTypeDef IWDG_Handler; //独立看门狗句柄
 
@@ -18,16 +19,21 @@ IWDG_HandleTypeDef IWDG_Handler; //独立看门狗句柄
 //prer:分频数:IWDG_PRESCALER_4~IWDG_PRESCALER_256
 //rlr:自动重装载值,0~0XFFF.
 //时间计算(大概):Tout=((4*2^prer)*rlr)/32 (ms).
-void IWDG_Init(u8 prer,u16 rlr)
+// eg: IWDG_Init(IWDG_PRESCALER_64,500);  	//分频数为64,重载值为500,溢出时间为1s
+int IWDG_Init(uint8_t prer, uint16_t rlr)
 {
-    IWDG_Handler.Instance=IWDG;
-    IWDG_Handler.Init.Prescaler=prer;	//设置IWDG分频系数
-    IWDG_Handler.Init.Reload=rlr;		//重装载值
-    HAL_IWDG_Init(&IWDG_Handler);		//初始化IWDG,默认会开启独立看门狗	
+    IWDG_Handler.Instance = IWDG;
+    IWDG_Handler.Init.Prescaler = prer; //设置IWDG分频系数
+    IWDG_Handler.Init.Reload = rlr;     //重装载值
+    HAL_IWDG_Init(&IWDG_Handler);       //初始化IWDG,默认会开启独立看门狗
+
+    return 0;
 }
-    
+
+// INIT_BOARD_EXPORT(IWDG_Init);
+
 //喂独立看门狗
 void IWDG_Feed(void)
-{   
-    HAL_IWDG_Refresh(&IWDG_Handler); 	//喂狗
+{
+    HAL_IWDG_Refresh(&IWDG_Handler); //喂狗
 }
